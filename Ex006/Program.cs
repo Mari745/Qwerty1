3aa1bed18df6a10a8d7138f8283fd361630660a3
Maria <maffulia@gmail.com>
2023-01-07 10:21:58 +0300

    Добавлен Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+while (n <= N)
+{
+    if(n % 2==0)
+
+    Console.WriteLine(n + ", ");
+    n ++;
+}
