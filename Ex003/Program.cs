9e8eff5122dab74bac42f13008b221bb42cec092
Maria <maffulia@gmail.com>
2023-01-07 11:39:02 +0300

    Добавлена задача 1 и блок-схема к ней

---
+++ Ex003/Program.cs
@@ -0,0 +1,17 @@
+﻿int a, b;
+Console.WriteLine("Введите два числа:");
+a = 10;
+b = 2;
+
+if ( a > b )
+{
+   Console.WriteLine("{0} максимальное", a);
+   Console.WriteLine("{0} минимальное" , b);
+}
+
+else
+{
+   Console.WriteLine("{0} минимальное", b);
+   Console.WriteLine("{0} максимальное", a);
+}
+Console.ReadLine();
---
+++ Ex005/Program.cs
@@ -0,0 +1,5 @@
+﻿double number 4, -3 , 7 ;
+if (number/2);
+console.WriteLine(number/2);
+
+
